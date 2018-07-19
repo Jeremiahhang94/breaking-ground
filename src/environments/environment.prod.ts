8af6e94a865ed2d5c9458685ac6ebedaa857985e
hope-uni <longan94@gmail.com>
2018-07-19 19:36:43 +0800

    set up deploy for new domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  redirectUri: "https://jeremiah-ang.github.io/hopeuni-camp/redirect",
+  redirectUri: "https://hope-uni.github.io/breaking-ground/redirect",
   sheetId: "1tPlvjGccIiwq-kWWe2G0LUVNAitdBqlo8n8LbF98ePE",
-  logoSrc: "/hopeuni-camp/assets/images/background.png",
+  logoSrc: "/breaking-ground/assets/images/background.png",
   trackingId: "UA-122498528-1"
 };
