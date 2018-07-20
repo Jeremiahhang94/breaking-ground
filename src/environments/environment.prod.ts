1ab12aebddcb3b56999e392b026e649cc303558a
hope-uni <jeremiah@visenze.com>
2018-07-20 12:12:54 +0800

    update apikey for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   redirectUri: "https://hope-uni.github.io/breaking-ground/redirect",
   sheetId: "1tPlvjGccIiwq-kWWe2G0LUVNAitdBqlo8n8LbF98ePE",
   logoSrc: "/breaking-ground/assets/images/background.png",
+  apiKey: "AIzaSyCmxhEvT-RnwYYoVWD-WPWjC-ByCnJj_xw",
   trackingId: "UA-122498528-1"
 };
